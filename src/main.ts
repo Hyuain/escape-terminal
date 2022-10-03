fb913dc4184e88cf42756138e0fba14ec5be1784
Hyuain <Hyuain97@163.com>
2022-10-03 15:16:38 +0800

    import vue router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import './style.css'
 import { App } from './App'
+import { createRouter } from 'vue-router'
+import { routes } from './config/routes'
+import { history } from './shared/history'
 
-createApp(App).mount('#app')
+const router = createRouter({
+  routes, history,
+})
+
+createApp(App)
+  // .use(router)
+  .mount('#app')
