f1877e6c543ed791b28cbfc547cc3d08a8efac17
Hyuain <Hyuain97@163.com>
2022-10-06 17:42:34 +0800

    delete signout page

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,11 +1,9 @@
 import { RouteRecordRaw } from 'vue-router'
 import { SignIn } from '../view/SignIn/SignIn'
-import { SignUp } from '../view/SignUp/SignUp'
 import { Home } from '../view/Home/Home'
 
 export const routes: RouteRecordRaw[] = [
   { path: '/sign_in', component: SignIn },
-  { path: '/sign_up', component: SignUp },
   { path: '/home', component: Home },
   { path: '/', component: SignIn }
 ]
