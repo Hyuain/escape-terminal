c9b1198a17d6e16872baa3c640941da69c559502
Hyuain <Hyuain97@163.com>
2022-10-26 16:29:03 +0800

    socket connected

--- src/view/Chat/ChatRoom/ChatRoom.tsx
+++ src/view/Chat/ChatRoom/ChatRoom.tsx
@@ -7,6 +7,22 @@ import s from './ChatRoom.module.scss'
 
 export const ChatRoom = defineComponent({
   setup() {
+    console.log('xxx')
+    const socket = new WebSocket('ws://localhost:3000/websocket')
+    socket.addEventListener('open', (x) => {
+      console.log('xxxOpen', x)
+      socket.send(JSON.stringify({
+        command: 'subscribe',
+        identifier: JSON.stringify({
+          channel: 'ChatChannel'
+        })
+      }))
+    })
+    socket.addEventListener('message', (e) => {
+      const message = JSON.parse(e.data)
+      if (message.type === 'ping') { return }
+      console.log('xxxMessage', message)
+    })
     return () => <PageWrapper>
       <Header title={'Name'}></Header>
       <BubbleList class={s.bubbleList}></BubbleList>
