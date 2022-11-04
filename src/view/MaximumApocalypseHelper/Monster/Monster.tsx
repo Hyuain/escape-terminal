e6bdc158f368f4b1e65e51f350fd8d72a99b3e04
Hyuain <Hyuain97@163.com>
2022-11-04 16:09:27 +0800

    add monster page

--- src/view/MaximumApocalypseHelper/Monster/Monster.tsx
+++ src/view/MaximumApocalypseHelper/Monster/Monster.tsx
@@ -1,9 +1,56 @@
 import { defineComponent } from 'vue'
+import { Header } from '../../../components/Header/Header'
+import { useRoute, useRouter } from 'vue-router'
+import { useMADataStore } from '../tools/dataManager'
+import { PageWrapper } from '../../../components/PageWrapper/PageWrapper'
+import { MonsterItem } from '../MonsterList/MonsterItem/MonsterItem'
+import { PlayerItem } from '../PlayerList/components/PlayerItem/PlayerItem'
 
 export const Monster = defineComponent({
   setup() {
-    return () => <div>
+    const route = useRoute()
+    const router = useRouter()
+    const dataStore = useMADataStore()
+    const maDataWrapper = dataStore.getWrapper()
+    const monsterId: string = route.query.id as string
+    const monster = dataStore.getOne('monsters', monsterId)
+    const hostPlayer = dataStore.getMonsterAttachedPlayer(monster?.id)
+    const hostPlayerMap = dataStore.getPlayerPosition(hostPlayer?.id)
+    const allPlayers = dataStore.getPlayersUsingMap(hostPlayerMap?.id)
 
-    </div>
-  }
+    const editMonsterHp = (delta: number) => {
+      if (!monster) { return }
+      if (monster.hp === undefined) {
+        monster.hp = monster.maxHp
+      }
+      monster.hp = monster.hp + delta
+      dataStore.update('players', monster)
+    }
+
+    return () => <PageWrapper>
+      <Header title={monster?.name || ''} onClickBack={() => router.back()}/>
+      {monster
+        ? <div>
+          <div>{monster.name}</div>
+          <div>
+            <div>HP: {monster.hp}</div>
+            <div onClick={() => editMonsterHp(-1)}>-</div>
+            <div onClick={() => editMonsterHp(+1)}>+</div>
+          </div>
+          <div>MaxHp: {monster.maxHp}</div>
+          <div>HostPlayer:</div>
+          <div>
+            <PlayerItem player={hostPlayer}/>
+          </div>
+          <div>AllPlayers:</div>
+          <div>
+            {allPlayers.map((player) => <PlayerItem
+              player={player}
+            />)}
+          </div>
+        </div>
+        : <div>No Data</div>
+      }
+    </PageWrapper>
+  },
 })
--- src/view/MaximumApocalypseHelper/PlayerList/components/PlayerItem/PlayerItem.tsx
+++ src/view/MaximumApocalypseHelper/PlayerList/components/PlayerItem/PlayerItem.tsx
@@ -2,22 +2,28 @@ import { defineComponent, PropType } from 'vue'
 import { IPlayer } from '../../../tools/dataManager.interface'
 import s from './PlayerItem.module.scss'
 import { MAHelperAvatar } from '../../../components/MAHelperAvatar/MAHelperAvatar'
+import { useMADataStore } from '../../../tools/dataManager'
 
 export const PlayerItem = defineComponent({
   props: {
     player: Object as PropType<IPlayer>,
+    playerId: String,
     isSelected: Boolean,
     isShowMonsters: Boolean,
   },
   emits: ['addPlayer', 'clickPlayer', 'addMonster', 'clickMonster'],
   setup(props, context) {
+    const dataStore = useMADataStore()
+
+    const player: IPlayer = props.player || dataStore.getOne('monsters', props.playerId) as IPlayer
+
     return () => <div class={{ [`${s.item}`]: true, [`${s.selected}`]: props.isSelected }}>
-      {props.player
+      {player
         ? <div onClick={() => context.emit('clickPlayer', props.player)}>
-          <div>{props.player.name}</div>
+          <div>{player.name}</div>
           {props.isShowMonsters
             ? <div>
-              {props.player.monsters?.map((monsterId) => <MAHelperAvatar
+              {player.monsters?.map((monsterId) => <MAHelperAvatar
                 type="monsters"
                 onClick={() => context.emit('clickMonster', monsterId)}
                 id={monsterId}/>)}
