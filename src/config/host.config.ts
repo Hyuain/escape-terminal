a4c5c73b513db1f88339f8b2e4a91f3f903a3023
Hyuain <Hyuain97@163.com>
2022-11-10 14:39:44 +0800

    add private host

--- src/config/host.config.ts
+++ src/config/host.config.ts
@@ -1,6 +1,9 @@
+import { hostPrivateConfig } from '@/config/host.private'
+
 export const hostConfig = {
   request: 'http://localhost:3000',
   websocket: 'ws://localhost:3000/websocket',
   // request: 'http://192.168.43.164:3000',
   // websocket: 'ws://192.168.43.164:3000/websocket',
+  ...hostPrivateConfig as any,
 }
